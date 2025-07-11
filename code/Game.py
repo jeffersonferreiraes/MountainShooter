4109326db709fbed9991162ee39b4e806ec3197b
Jefferson Alves <jealves359@gmail.com>
2025-07-10 21:51:29 -0300

    CREATING ASSET FOLDER INSERTING ALL ASSETS CREATING THE MENU: BACKGROUND, SOUND AND FONTS CREATING THE CONST FILE

--- code/Game.py
+++ code/Game.py
@@ -12,19 +12,14 @@ class Game:
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDHT, WIN_HEIGHT))
 
-    def run(self, ):
-        print('Setup start')
-        print('Setup end')
+    def run(self):
 
-        print('Loop start')
+        # print('Setup start')
+        # print('Setup end')
+        # print('Loop start')
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         print('QUITING...')
-            #         pygame.quit()  # close window
-            #         quit()  # end pygame
+
